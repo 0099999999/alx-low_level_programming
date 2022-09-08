391f0229b89d0f39b15232d7f094584f934d162f
0099999999 <catherinelangat25@gmail.com>
2022-09-08 15:52:53 -0700

    Unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,25 +1,16 @@
-#include <stdio.h> 
+#include <stdio.h>
+#include <unistd.h>
 
-  
+/**
+ * main - print quote
+ *
+ * Description: Korpar's quote
+ * 
+ * Return: Always 1 (Success)
+ */
 
- /** 
-  *
-  *   * main - Entry point 
-  *
-  *     * 
-  *
-  *       * Return: Always 0 (Success) 
-  *
-  *         */ 
-
- int main(void) 
-
-	 { 
-
-		          write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61); 
-
-			    
-
-			           return (1); 
-
-				    }
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
